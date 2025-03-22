147f3d4b8f8547e64499575ba73f0628dccf3cfb
Domingos <dmgssantiago@gmail.com>
2025-03-22 19:00:53 -0300

    Correção de rotas dos quizzes]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { QuizzesComponent } from './pages/quizzes/quizzes.component';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import quizData from '../assets/data/explore_quiz.json';
 
 
 export const routes: Routes = [
@@ -15,20 +14,9 @@ export const routes: Routes = [
     {
         path: 'quizzes',
         redirectTo: ''
-    },
-
-    {
-        path: 'quizzes/:id',
-        component: QuizzesComponent,
-        data: { prerender: true },
-        resolve: { prerenderParams: getPrerenderParams }
     }
 
 ];
-
-export function getPrerenderParams() {
-    return quizData.map(quiz => ({ id: quiz.id.toString() }));
-  }
 
 @NgModule({
     imports: [RouterModule.forRoot(routes), AppComponent],
